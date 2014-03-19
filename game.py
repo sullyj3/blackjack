64ed81167a1aac1822d3a01830415971e5a4b09f
James Sully <sullyj3@gmail.com>
2014-03-19 21:16:03 +1100

    added comment

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 cards=zip((["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']),([(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)])) #generates list of 2-tuples with form ("card-name",value)
 
-#deck=[foo[0] for foo in cards]
+#deck=[foo[0] for foo in cards] #seems to fuck up the for block on line 7
 
 #print(cards)
 
