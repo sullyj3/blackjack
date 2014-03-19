d9aa1571a4f294fa3ef50a7012383c9689d2110c
James Sully <sullyj3@gmail.com>
2014-03-19 22:11:35 +1100

    converted cards back to list.

--- game.py
+++ game.py
@@ -1,13 +1,10 @@
-cards=zip((["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']),([(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)])) #generates list of 2-tuples with form ("card-name",value)
+cards=list(zip((["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']),([(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)]))) #generates list of 2-tuples with form ("card-name",value)
 
 print(type(cards))
 #deck=[foo for foo in cards]
 #deck=[foo[0] for foo in cards] #seems to fuck up the for block on line 7
 
-#print(cards)
-
-for card in cards:
-    print(card)
+print(cards)
 
 def initial_deal():
     pass
