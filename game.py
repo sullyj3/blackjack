f3c481b51d4a8dcfdcd864ddd9b2284399f9b274
James Sully <sullyj3@gmail.com>
2014-03-19 21:07:37 +1100

    added a comment, cleaned up a bit

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
-cards=zip((["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']),([(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)]))
-deck=[foo[0] for foo in cards]
+cards=zip((["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']),([(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)])) #generates list of 2-tuples with form ("card-name",value)
+
+#deck=[foo[0] for foo in cards]
+
 #print(cards)
-print(deck)
-#print(["foo" for card in cards])
-print(cards)
+
 for card in cards:
     print(card)
-#def initial_deal():
- #   pass
+
+def initial_deal():
+    pass
