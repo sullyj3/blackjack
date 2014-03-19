a952477183d924da6ba63b21d5cbf59c0ccd24b9
James Sully <sullyj3@gmail.com>
2014-03-19 22:15:24 +1100

    fixed

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 cards=list(zip((["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']),([(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)]))) #generates list of 2-tuples with form ("card-name",value)
-
 print(type(cards))
-#deck=[foo for foo in cards]
-#deck=[foo[0] for foo in cards] #seems to fuck up the for block on line 7
-
 print(cards)
+
+deck=[card[0] for card in cards]
+print()
+print(type(deck))
+print(deck)
 
 def initial_deal():
     pass
