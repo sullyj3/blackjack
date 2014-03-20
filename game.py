99f0a7d6f80210e1ce80f374d7f7bccc6667dd8e
James Sully <sullyj3@gmail.com>
2014-03-20 19:41:40 +1100

    made cards a class rather than tuples

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from random import randrange
 
+'''
 def init_deck():  #initialise deck. returns list of 3-tuples of form: (name,face value,suit)
     #is this the best data structure?
     cardnames=["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']
@@ -9,6 +10,23 @@ def init_deck():  #initialise deck. returns list of 3-tuples of form: (name,face
     for suit in suits:
         for i in range(13):
             deck.append((cardnames[i],cardvals[i],suit))
+    return deck
+'''
+
+class card(object):
+    def __init__(self,name,face_value,suit):
+        self.name=name
+        self.face_value=face_value
+        self.suit=suit
+
+def init_deck():
+    cardnames=["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']
+    cardvals=[(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)]
+    suits=['hearts','clubs','diamonds','spades']
+    deck=[]
+    for suit in suits:
+        for i in range(13):
+            deck.append(card(cardnames[i],cardvals[i],suit))
     return deck
 
 def get_players(): #maybe should use classes instead?
