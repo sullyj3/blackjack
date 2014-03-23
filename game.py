0b369a2ef0e22f6782f1bb21ec7c248bcdcb13f6
James Sully <sullyj3@gmail.com>
2014-03-23 18:24:34 +1100

    logical reordering of definitions

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from random import randrange
 
+#class definitions
 class card(object):
     def __init__(self,name,face_value,suit):
         self.name=name
@@ -11,6 +12,8 @@ class player(object):
         self.name=name
         self.current_hand=current_hand
 
+#Function declarations
+#initialisation:
 def init_deck(): #returns a dictionary of card objects with key "cardname_suit"
     cardnames=["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']
     cardvals=[(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)]
@@ -21,7 +24,6 @@ def init_deck(): #returns a dictionary of card objects with key "cardname_suit"
             #deck.append(card(cardnames[i],cardvals[i],suit))
             deck[cardnames[i]+"_"+suit]=card(cardnames[i],cardvals[i],suit)
     return deck
-
 def get_players(): #returns list of player names
     players=[]
     num_players=int(input("Input number of players: "))
@@ -29,13 +31,13 @@ def get_players(): #returns list of player names
         add_player=input("Input player "+str(i+1)+" name: ")
         players.append(add_player)
     return players
-
 def generate_players(name_list): #takes a list of names as strings, returns a dict of player objects
     players={}
     for name in name_list:
         players[name]=player(name,[])
     return players
 
+#minor game tasks
 def shuffle_deck(deck): # takes and returns a list of cards
     temp_deck=deck
     shuffled_deck=[]
@@ -46,21 +48,20 @@ def shuffle_deck(deck): # takes and returns a list of cards
         del temp_deck[random_index]
 
     return shuffled_deck
-
 def deal_card(deck,player): #make sure deck is shuffled before calling this
     #deck should be a list, player should be a player object
     (player.current_hand).append(deck.pop(0)) #pop() removes card from deck
-
-def round(players,deck): #will be main function
-    initial_deal(players)
-
 def initial_deal(players): #takes list of player objects, updates current_hand attribute
     for player in players:
         deal_card(main_deck, player)        
         deal_card(main_deck, player)        
 
+#major game control structures
+def round(players,deck): #will be main function
+    initial_deal(players)
+
+#important variable assignments
 deck_dict=init_deck()
 deck_list=[x for x in deck_dict.values()]
 main_deck=shuffle_deck(deck_list)
-
 players=generate_players(get_players())
