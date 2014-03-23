1f4b42faeb8155d1e98ce5254473abcfc001ecd9
James Sully <sullyj3@gmail.com>
2014-03-23 14:13:35 +1100

    commented out the first init deck def properly

--- game.py
+++ game.py
@@ -1,17 +1,15 @@
 from random import randrange
 
-'''
-def init_deck():  #initialise deck. returns list of 3-tuples of form: (name,face value,suit)
-    #is this the best data structure?
-    cardnames=["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']
-    cardvals=[(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)]
-    suits=['hearts','clubs','diamonds','spades']
-    deck=[]
-    for suit in suits:
-        for i in range(13):
-            deck.append((cardnames[i],cardvals[i],suit))
-    return deck
-'''
+#def init_deck():  #initialise deck. returns list of 3-tuples of form: (name,face value,suit)
+#    #is this the best data structure?
+#    cardnames=["ace"]+[str(i) for i in range(2,11)]+['jack','queen','king']
+#    cardvals=[(1,11)]+[i for i in range(2,11)]+[10 for i in range(3)]
+#    suits=['hearts','clubs','diamonds','spades']
+#    deck=[]
+#    for suit in suits:
+#        for i in range(13):
+#            deck.append((cardnames[i],cardvals[i],suit))
+#    return deck
 
 class card(object):
     def __init__(self,name,face_value,suit):
