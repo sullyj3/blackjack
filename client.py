79d8bd25dd83a65dfc7cf49989948496eb57ebd9
James Sully <sullyj3@gmail.com>
2015-05-28 07:43:18 +1000

    more decoupling

--- client.py
+++ client.py
@@ -14,3 +14,17 @@ def generate_players(name_list): #takes a list of names as strings, returns a di
         players[name] = Player(name,[])
     return players
 
+def standorhit(player): #takes a Player object, updates player.standing attribute. returns decision
+    decision = ''
+    print("Will "+player.name+" (s)tand or take a (h)it?")
+    #keep asking till valid input
+    while not decision in ('h','s'):
+        decision = input('> ').lower()
+        if not decision in ('h','s'):
+            print("please enter either 's' or 'h'")
+    if decision=='h':
+        player.standing = False
+    elif decision=='s':
+        player.standing = True
+    return decision
+
